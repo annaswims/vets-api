3f2236e7a8d9a1e30635d4ea29b778127d65d767
Laura Trager <laura.trager@adhocteam.us>
2022-06-30 11:16:53 -0400

    Attempt to fix schema spec failure

--- modules/appeals_api/spec/support/schema_helpers.rb
+++ modules/appeals_api/spec/support/schema_helpers.rb
@@ -16,11 +16,25 @@ module SchemaHelpers
     )
   end
 
+  def resolver
+    proc do |uri|
+      json_file = uri.path
+      parsed_schema = JSON.parse File.read shared_dir(json_file)
+      parsed_schema['properties'].values.first
+    end
+  end
+
   def override_max_lengths(appeal, schema)
     JSONSchemer.schema(schema,
                        after_property_validation: proc do |data, property, property_schema, _parent|
                          data[property] = 'W' * property_schema['maxLength'] if property_schema['maxLength']
                        end).valid?(appeal.form_data)
     appeal.form_data
+  end
+
+  private
+
+  def shared_dir(file)
+    Rails.root.join('modules', 'appeals_api', Settings.modules_appeals_api.schema_dir, 'shared', 'v1', file)
   end
 end
