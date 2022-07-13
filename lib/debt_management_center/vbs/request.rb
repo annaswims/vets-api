06a0475e5fbfc95b4ed033f143ae98c245033a1a
Scott James <scott.james@gcio.com>
2022-07-13 16:34:41 -0400

    Add Generate payload method

--- lib/debt_management_center/vbs/request.rb
+++ lib/debt_management_center/vbs/request.rb
@@ -82,6 +82,220 @@ module DebtManagementCenter
         }
       end
 
+      def generate_payload
+        {
+          "personalIdentification" => {
+              "ssn" => Faker::Number.number(digits: 10),
+              "fileNumber" => Faker::Number.number(digits: 10),
+              "fsrReason" => Faker::Lorem.word
+          },
+          "personalData" => {
+              "veteranFullName" => {
+                  "first" => Faker::Name.first_name,
+                  "middle" => Faker::Name.initials(number: 1),
+                  "last" => Faker::Name.last_name
+              },
+              "address" => {
+                  "addresslineOne" => Faker::Address.street_address,
+                  "addresslineTwo" => Faker::Address.secondary_address,
+                  "addresslineThree" => '',
+                  "city" => Faker::Address.city,
+                  "stateOrProvince" => Faker::Address.state_abbr,
+                  "zipOrPostalCode" => Faker::Address.zip,
+                  "countryName" => Faker::Address.country
+              },
+              "telephoneNumber" => Faker::PhoneNumber.cell_phone,
+              "emailAddress" => Faker::Internet.email,
+              "dateOfBirth" => "#{Faker::Number.between(from: 1, to: 12)}/#{Faker::Number.between(from: 1, to: 28)}/#{Faker::Number.between(from: 1930, to: 2004)}",
+              "married" => true,
+              "spouseFullName" => {
+                  "first" => Faker::Name.first_name,
+                  "middle" => Faker::Name.initials(number: 1),
+                  "last" => Faker::Name.last_name
+              },
+              "agesOfOtherDependents" => [
+                  Faker::Number.between(from: 1, to: 17),
+                  Faker::Number.between(from: 1, to: 17)
+              ],
+              "employmentHistory" => [
+                  {
+                      "veteranOrSpouse" => "VETERAN",
+                      "occupationName" => Faker::Job.title,
+                      "from" => "#{Faker::Number.between(from: 1, to: 12)}/#{Faker::Number.between(from: 2015, to: 2021)}",
+                      "to" => "",
+                      "present" => true,
+                      "employerName" => Faker::Company.name,
+                      "employerAddress" => {
+                        "addresslineOne" => Faker::Address.street_address,
+                        "addresslineTwo" => Faker::Address.secondary_address,
+                        "addresslineThree" => '',
+                        "city" => Faker::Address.city,
+                        "stateOrProvince" => Faker::Address.state_abbr,
+                        "zipOrPostalCode" => Faker::Address.zip,
+                        "countryName" => Faker::Address.country
+                      }
+                  },
+                  {
+                      "veteranOrSpouse" => "VETERAN",
+                      "occupationName" => Faker::Job.title,
+                      "from" => "#{Faker::Number.between(from: 1, to: 12)}/#{Faker::Number.between(from: 2000, to: 2014)}",
+                      "to" => "#{Faker::Number.between(from: 1, to: 12)}/2015",
+                      "present" => false,
+                      "employerName" => Faker::Company.name,
+                      "employerAddress" => {
+                        "addresslineOne" => Faker::Address.street_address,
+                        "addresslineTwo" => Faker::Address.secondary_address,
+                        "addresslineThree" => '',
+                        "city" => Faker::Address.city,
+                        "stateOrProvince" => Faker::Address.state_abbr,
+                        "zipOrPostalCode" => Faker::Address.zip,
+                        "countryName" => Faker::Address.country
+                      }
+                  },
+                  {
+                      "veteranOrSpouse" => "SPOUSE",
+                      "occupationName" => Faker::Job.title,
+                      "from" => "#{Faker::Number.between(from: 1, to: 12)}/#{Faker::Number.between(from: 2015, to: 2021)}",
+                      "to" => "",
+                      "present" => true,
+                      "employerName" => Faker::Company.name,
+                      "employerAddress" => {
+                        "addresslineOne" => Faker::Address.street_address,
+                        "addresslineTwo" => Faker::Address.secondary_address,
+                        "addresslineThree" => '',
+                        "city" => Faker::Address.city,
+                        "stateOrProvince" => Faker::Address.state_abbr,
+                        "zipOrPostalCode" => Faker::Address.zip,
+                        "countryName" => Faker::Address.country
+                      }
+                  }
+              ]
+          },
+          "income" => [
+              {
+                  "veteranOrSpouse" => "VETERAN",
+                  "monthlyGrossSalary" => Faker::Number.number(digits: 4),
+                  "deductions" => {
+                      "taxes" => Faker::Number.number(digits: 3),
+                      "retirement" => Faker::Number.number(digits: 3),
+                      "socialSecurity" => Faker::Number.number(digits: 3),
+                      "otherDeductions" => {
+                          "name" => Faker::Lorem.sentence,
+                          "amount" => Faker::Number.number(digits: 3)
+                      }
+                  },
+                  "totalDeductions" => Faker::Number.number(digits: 3),
+                  "netTakeHomePay" => Faker::Number.number(digits: 5),
+                  "otherIncome" => {
+                      "name" => Faker::Lorem.sentence,
+                      "amount" =>Faker::Number.number(digits: 3)
+                  },
+                  "totalMonthlyNetIncome" => Faker::Number.number(digits: 4)
+              },
+              {
+                  "veteranOrSpouse" => "SPOUSE",
+                  "monthlyGrossSalary" => Faker::Number.number(digits: 4),
+                  "deductions" => {
+                      "taxes" => Faker::Number.number(digits: 3),
+                      "retirement" => Faker::Number.number(digits: 3),
+                      "socialSecurity" => Faker::Number.number(digits: 3),
+                      "otherDeductions" => {
+                          "name" => Faker::Lorem.sentence,
+                          "amount" => Faker::Number.number(digits: 3)
+                      }
+                  },
+                  "totalDeductions" => Faker::Number.number(digits: 3),
+                  "netTakeHomePay" => Faker::Number.number(digits: 5),
+                  "otherIncome" => {
+                      "name" => Faker::Lorem.sentence,
+                      "amount" =>Faker::Number.number(digits: 3)
+                  },
+                  "totalMonthlyNetIncome" => Faker::Number.number(digits: 4)
+              }
+          ],
+          "expenses" => {
+              "rentOrMortgage" => Faker::Number.number(digits: 3),
+              "food" => Faker::Number.number(digits: 3),
+              "utilities" => Faker::Number.number(digits: 3),
+              "otherLivingExpenses" => {
+                  "name" => Faker::Lorem.sentence,
+                  "amount" => Faker::Number.number(digits: 3)
+              },
+              "expensesInstallmentContractsAndOtherDebts" => Faker::Number.number(digits: 3),
+              "totalMonthlyExpenses" => Faker::Number.number(digits: 4)
+          },
+          "discretionaryIncome" => {
+              "netMonthlyIncomeLessExpenses" => Faker::Number.number(digits: 4),
+              "amountCanBePaidTowardDebt" => Faker::Number.number(digits: 3)
+          },
+          "assets" => {
+              "cashInBank" => Faker::Number.number(digits: 5),
+              "cashOnHand" => Faker::Number.number(digits: 3),
+              "automobiles" => [
+                  {
+                      "make" => Faker::Vehicle.make,
+                      "model" => Faker::Vehicle.model,
+                      "year" => Faker::Number.between(from: 1960, to: 2020),
+                      "resaleValue" => Faker::Number.number(digits: 4)
+                  },
+                  {
+
+                  }
+              ],
+              "trailersBoatsCampers" => Faker::Number.number(digits: 3),
+              "usSavingsBonds" => Faker::Number.number(digits: 3),
+              "stocksAndOtherBonds" => Faker::Number.number(digits: 4),
+              "realEstateOwned" => Faker::Number.number(digits: 6),
+              "otherAssets" => [
+                  {
+                      "name" => Faker::Lorem.word,
+                      "amount" => Faker::Number.number(digits: 3)
+                  }
+              ],
+              "totalAssets" => Faker::Number.number(digits: 6)
+          },
+          "installmentContractsAndOtherDebts" => [
+              {
+                  "creditorName" => Faker::Bank.name,
+                  "creditorAddress" => {
+                    "addresslineOne" => Faker::Address.street_address,
+                    "addresslineTwo" => Faker::Address.secondary_address,
+                    "addresslineThree" => '',
+                    "city" => Faker::Address.city,
+                    "stateOrProvince" => Faker::Address.state_abbr,
+                    "zipOrPostalCode" => Faker::Address.zip,
+                    "countryName" => Faker::Address.country
+                  },
+                  "dateStarted" => "#{Faker::Number.between(from: 1, to: 12)}/#{Faker::Number.between(from: 1, to: 28)}/#{Faker::Number.between(from: 1990, to: 2022)}",
+                  "purpose" => Faker::Lorem.sentence,
+                  "originalAmount" => Faker::Number.number(digits: 5),
+                  "unpaidBalance" => Faker::Number.number(digits: 4),
+                  "amountDueMonthly" => Faker::Number.number(digits: 3),
+                  "amountPastDue" => Faker::Number.number(digits: 2)
+              }
+          ],
+          "totalOfInstallmentContractsAndOtherDebts" => {
+            "originalAmount" => Faker::Number.number(digits: 5),
+            "unpaidBalance" => Faker::Number.number(digits: 4),
+            "amountDueMonthly" => Faker::Number.number(digits: 3),
+            "amountPastDue" => Faker::Number.number(digits: 2)
+          },
+          "additionalData" => {
+              "bankruptcy" => {
+                  "hasBeenAdjudicatedBankrupt" => true,
+                  "dateDischarged" => "#{Faker::Number.between(from: 1, to: 12)}/#{Faker::Number.between(from: 1, to: 28)}/#{Faker::Number.between(from: 1990, to: 2022)}",
+                  "courtLocation" => Faker::Address.city,
+                  "docketNumber" => Faker::Number.number(digits: 8)
+              },
+              "additionalComments" => Faker::Lorem.paragraph
+          },
+          "applicantCertifications" => {
+              "veteranSignature" => Faker::Name.name,
+              "veteranDateSigned" => "06/#{Faker::Number.between(from: 1, to: 28)}/2022"
+          }
+      }
+      end
+
       ##
       # Betamocks enabled status from settings
       #
