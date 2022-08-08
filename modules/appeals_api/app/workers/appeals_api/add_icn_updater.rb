813b00b3745720e9368340fa538bb143818713f2
Dan Hinze <hinzed1127@gmail.com>
2022-08-08 14:13:40 -0500

    Linting

--- modules/appeals_api/app/workers/appeals_api/add_icn_updater.rb
+++ modules/appeals_api/app/workers/appeals_api/add_icn_updater.rb
@@ -11,21 +11,22 @@ module AppealsApi
     end
 
     private
-      def target_veteran
-        veteran ||= Appellant.new(
-          type: :veteran,
-          auth_headers: request_headers,
-          form_data: submission.form_data&.dig('data', 'attributes', 'veteran')
-        )
-    
-        mpi_veteran ||= AppealsApi::Veteran.new(
-          ssn: veteran.ssn,
-          first_name: veteran.first_name,
-          last_name: veteran.last_name,
-          birth_date: veteran.birth_date.iso8601
-        )
-    
-        mpi_veteran
-      end
+
+    def target_veteran
+      veteran ||= Appellant.new(
+        type: :veteran,
+        auth_headers: request_headers,
+        form_data: submission.form_data&.dig('data', 'attributes', 'veteran')
+      )
+
+      mpi_veteran ||= AppealsApi::Veteran.new(
+        ssn: veteran.ssn,
+        first_name: veteran.first_name,
+        last_name: veteran.last_name,
+        birth_date: veteran.birth_date.iso8601
+      )
+
+      mpi_veteran
+    end
   end
 end
