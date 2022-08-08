0d5bf974ae0f91970cdb5ffd0c3b41928c901461
Dan Hinze <hinzed1127@gmail.com>
2022-08-08 13:46:23 -0500

    Add sidekiq job to update submissions with ICN values

---
+++ modules/appeals_api/app/workers/appeals_api/add_icn_updater.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'sidekiq'
+
+module AppealsApi
+  class AddIcnUpdater
+    include Sidekiq::Worker
+
+    def perform(submission)
+      submission.update!(vetearn_icn: target_veteran.mpi_icn)
+    end
+
+    private
+      def target_veteran
+        veteran ||= Appellant.new(
+          type: :veteran,
+          auth_headers: request_headers,
+          form_data: submission.form_data&.dig('data', 'attributes', 'veteran')
+        )
+    
+        mpi_veteran ||= AppealsApi::Veteran.new(
+          ssn: veteran.ssn,
+          first_name: veteran.first_name,
+          last_name: veteran.last_name,
+          birth_date: veteran.birth_date.iso8601
+        )
+    
+        mpi_veteran
+      end
+  end
+end
