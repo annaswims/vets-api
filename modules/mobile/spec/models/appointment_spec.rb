535f7b6dd340039ddd928afb3abf12be81befe80
andrewherzberg <aherzberg5@gmail.com>
2022-08-19 10:24:54 -0700

    revert 92b6a09f4323f88fbd47fbf293e4fdc053cb98c7

--- modules/mobile/spec/models/appointment_spec.rb
+++ modules/mobile/spec/models/appointment_spec.rb
@@ -3,30 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Mobile::V0::Appointment, type: :model do
-  describe '.convert_non_prod_id!' do
-    it 'converts mocked ids to real ones' do
-      expect(Mobile::V0::Appointment.convert_non_prod_id!('983')).to eq('442')
+  describe '.toggle_non_prod_id!' do
+    it 'toggles mocked ids to real ones' do
+      expect(Mobile::V0::Appointment.toggle_non_prod_id!('983')).to eq('442')
     end
 
-    it 'does not change id when already valid' do
-      expect(Mobile::V0::Appointment.convert_non_prod_id!('442')).to eq('442')
+    it 'toggles real ids back to mocked ones' do
+      expect(Mobile::V0::Appointment.toggle_non_prod_id!('442')).to eq('983')
     end
 
     it 'keeps secondary identifiers' do
-      expect(Mobile::V0::Appointment.convert_non_prod_id!('983GC')).to eq('442GC')
-    end
-
-    context 'when in production' do
-      before do
-        @original_hostname = Settings.hostname
-        Settings.hostname = 'api.va.gov'
-      end
-
-      after { Settings.hostname = @original_hostname }
-
-      it 'does not convert mocked ids' do
-        expect(Mobile::V0::Appointment.convert_non_prod_id!('983')).to eq('983')
-      end
+      expect(Mobile::V0::Appointment.toggle_non_prod_id!('983GC')).to eq('442GC')
     end
   end
 end
