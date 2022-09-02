ec78a1092d74d0ddd8652da37282a4c70f7a0cea
Dan Hinze <hinzed1127@gmail.com>
2022-09-01 22:23:36 -0500

    Clean up VeteranWithMPIAttributes
    Add some clarifying comments to a few methods, and remove a
    method no longer needed on the class.

--- modules/appeals_api/app/models/appeals_api/veteran_with_mpi_attributes.rb
+++ modules/appeals_api/app/models/appeals_api/veteran_with_mpi_attributes.rb
@@ -5,18 +5,15 @@
 # in lieu of an ssn.
 module AppealsApi
   class VeteranWithMPIAttributes < Appellant
+    # address is a private method on AppealsApi::Appellant.
+    # We need it public for querying MPI
     def address
       form_data['address'] || {}
     end
 
+    # Override the inherited method to match expected formats for MPI
     def birth_date
       auth_headers['X-VA-Birth-Date']
-    end
-
-    # Required by MPI when validating in message_user_attributes.
-    # (See lib/mpi/service.rb)
-    def valid?
-      true
     end
 
     # We don't take the following attributes in for NOD,
