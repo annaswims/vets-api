c04b4bca5e2d8f27abb806470f1ba7a96e1d3442
Kha Huynh <kha.huynh@thoughtworks.com>
2022-09-12 15:52:51 -0400

    [department-of-veterans-affairs/va-virtual-agent#615] Add changes back to write claims & appeals records to db
    Co-Authored-By: justin-trieu <89413920+justin-trieu@users.noreply.github.com>
    Co-Authored-By: aaronyoung-tw <109609223+aaronyoung-tw@users.noreply.github.com>

---
+++ spec/factories/create_virtual_agent_user_access_records.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :create_virtual_agent_user_access_record do
+    action_type { "MyString" }
+    first_name { "MyString" }
+    last_name { "MyString" }
+    ssn { "MyString" }
+    icn { "MyString" }
+  end
+end
