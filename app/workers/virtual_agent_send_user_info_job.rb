d975610209f2f5a10f5dbcbf6430b4211f418912
Kha Huynh <kha.huynh@thoughtworks.com>
2022-09-15 10:58:41 -0400

    [department-of-veterans-affairs/va-virtual-agent#615] Add test for january month
    Co-Authored-By: justin-trieu <89413920+justin-trieu@users.noreply.github.com>

--- app/workers/virtual_agent_send_user_info_job.rb
+++ app/workers/virtual_agent_send_user_info_job.rb
@@ -6,11 +6,13 @@ class VirtualAgentSendUserInfoJob
   def perform()
     current_datetime = Time.now
     month_int = current_datetime.month - 1
-    month = Date::ABBR_MONTHNAMES[month_int]
     year = current_datetime.year
+    if month_int == 0
+      month_int = 12
+      year = current_datetime.year - 1
+    end
+    month = Date::ABBR_MONTHNAMES[month_int]
 
-    puts month_int
-    puts 'hello'
     csv_string = CSV.generate do |csv|
       csv << VirtualAgentUserAccessRecord.attribute_names
       VirtualAgentUserAccessRecord.find_by_sql("SELECT * FROM virtual_agent_user_access_records WHERE EXTRACT(month FROM created_at) = #{month_int}").to_a.each do |user_record|
--- modules/appeals_api/spec/workers/virtual_agent_user_info_job_spec.rb
+++ modules/appeals_api/spec/workers/virtual_agent_user_info_job_spec.rb
@@ -8,29 +8,36 @@ describe VirtualAgentSendUserInfoJob, type: :job do
 
     before(:each) do 
       Timecop.freeze(Time.local(2022, 9, 1))
-      user_record_1 = FactoryBot.create(:virtual_agent_user_access_record,first_name: 'Rob')
-      user_record_2 = FactoryBot.create(:virtual_agent_user_access_record, id: '2', first_name: 'Tracy', created_at: '2022-08-01 20:08:42.300961')
-      user_record_3 = FactoryBot.create(:virtual_agent_user_access_record, id: '3', first_name: 'Betty', created_at: '2022-04-20 20:08:42.300961')
-      user_record_4 = FactoryBot.create(:virtual_agent_user_access_record, id: '4', first_name: 'Walter', created_at: '2022-08-15 20:08:42.300961')
+      FactoryBot.create(:virtual_agent_user_access_record,first_name: 'Rob')
+      FactoryBot.create(:virtual_agent_user_access_record, id: '2', first_name: 'Tracy', created_at: '2022-08-01 20:08:42.300961')
+      FactoryBot.create(:virtual_agent_user_access_record, id: '3', first_name: 'Betty', created_at: '2022-04-20 20:08:42.300961')
+      FactoryBot.create(:virtual_agent_user_access_record, id: '4', first_name: 'Walter', created_at: '2022-08-15 20:08:42.300961')
 
     end 
 
-    it 'reads from the user records table' do
-       allow(VirtualAgentUserAccessRecord).to receive(:find_by_sql)
-
-       expect(VirtualAgentUserAccessRecord).to receive(:find_by_sql)
-       VirtualAgentSendUserInfoJob.new.perform()
-
-    end
-
-    it 'returns request object' do
-
+    it 'filters records by the target month and returns request object' do
       expected_csv_as_string = "id,action_type,first_name,last_name,ssn,icn,created_at,updated_at\n1,action_type,Rob,last_name,ssn,icn,2022-08-12 20:08:42 UTC,2022-09-12 20:08:42 UTC\n2,action_type,Tracy,last_name,ssn,icn,2022-08-01 20:08:42 UTC,2022-09-12 20:08:42 UTC\n4,action_type,Walter,last_name,ssn,icn,2022-08-15 20:08:42 UTC,2022-09-12 20:08:42 UTC\n"
       
       expected_request_object = { 'filename': 'chatbot-claims-appeals-Aug-2022.csv', 'payload': expected_csv_as_string }
       
       expect(VirtualAgentSendUserInfoJob.new.perform).to eq(expected_request_object)
     end
+
+    it 'handles edge case for first month of the year and returns request object' do
+      Timecop.freeze(Time.local(2022, 01, 1))
+      FactoryBot.create(:virtual_agent_user_access_record, id: '5', first_name: 'John', created_at: '2021-12-07 20:08:42.300961')
+      FactoryBot.create(:virtual_agent_user_access_record, id: '6', first_name: 'Lennon', created_at: '2022-02-20 20:08:42.300961')
+      FactoryBot.create(:virtual_agent_user_access_record, id: '7', first_name: 'Paul', created_at: '2021-12-15 20:08:42.300961')
+      FactoryBot.create(:virtual_agent_user_access_record, id: '8', first_name: 'McCartney', created_at: '2021-12-20 20:08:42.300961')
+
+
+      expected_csv_as_string = "id,action_type,first_name,last_name,ssn,icn,created_at,updated_at\n5,action_type,John,last_name,ssn,icn,2021-12-07 20:08:42 UTC,2022-09-12 20:08:42 UTC\n7,action_type,Paul,last_name,ssn,icn,2021-12-15 20:08:42 UTC,2022-09-12 20:08:42 UTC\n8,action_type,McCartney,last_name,ssn,icn,2021-12-20 20:08:42 UTC,2022-09-12 20:08:42 UTC\n"
+      
+      expected_request_object = { 'filename': 'chatbot-claims-appeals-Dec-2021.csv', 'payload': expected_csv_as_string }
+      
+      expect(VirtualAgentSendUserInfoJob.new.perform).to eq(expected_request_object)
+    end
+
   end
 
   # TODO: address edge case with January - 1
