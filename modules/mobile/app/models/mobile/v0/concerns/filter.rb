2d79cb28fe3cdbeb49c4c7cdf01b5b8457e5422e
Kris Pethtel <kris.pethtel@gmail.com>
2022-09-28 18:13:26 -0400

    wip

--- modules/mobile/app/models/mobile/v0/concerns/filter.rb
+++ modules/mobile/app/models/mobile/v0/concerns/filter.rb
@@ -4,7 +4,7 @@ module Mobile
   module V0
     module Concerns
       class Filter
-        PERMITTED_OPERATIONS = %w[orEqual andEqual notEqual].freeze
+        PERMITTED_OPERATIONS = %w[orEqual andEqual notEqual andInclude orInclude notInclude].freeze
         attr_reader :records
 
         def initialize(records)
@@ -26,19 +26,25 @@ module Mobile
           filterer.records
         end
 
+        private
+
         def gather_filters(filters)
           filters.each_with_object({}) do |requested_params, received_filters|
-            filter_name = requested_params.keys.first
-            operation = requested_params[filter_name].keys.first
-            search_term = requested_params[filter_name][operation]
+            attribute = requested_params.keys.first
+            operation = requested_params[attribute].keys.first
+            search_term = requested_params[attribute][operation]
             filter_pair = { operation: operation, search_term: search_term }
 
-            if received_filters.key?(filter_name)
-              received_filters[filter_name] << filter_pair
+            if received_filters.key?(attribute)
+              received_filters[attribute] << filter_pair
             else
-              received_filters[filtern_name] = [filter_pair]
+              received_filters[attribute] = [filter_pair]
             end
           end
+        end
+
+        def filters_valid?(filters)
+          # unclear exactly what the rules would be yet. 
         end
 
         def validate_request(filters)
@@ -50,9 +56,21 @@ module Mobile
           raise 'BAD FILTERS' unless filters.values.all? { |v| v.class.in?([String, Integer]) }
         end
 
-        def filter_records(_filters)
-          records.filter! { |r| r[k.to_sym] == v }
+        # needs to be per operation
+        def filter_records(filters)
+          filters.each_pair do |attribute, details|
+            operation = details[:operation]
+            value = details[:value]
+
+            case operation
+            when 'andEqual'
+              @records.filter! { |r| r[k.to_sym] == v }
+            when 'orEqual'
+              @records.f
+            end
+          end
         end
+
       end
     end
   end
