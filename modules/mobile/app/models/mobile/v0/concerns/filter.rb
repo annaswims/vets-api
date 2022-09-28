3915b8eb9674bd3d1cd3546db6f8668e49535192
Kris Pethtel <kris.pethtel@gmail.com>
2022-09-28 09:38:42 -0400

    wip

--- modules/mobile/app/models/mobile/v0/concerns/filter.rb
+++ modules/mobile/app/models/mobile/v0/concerns/filter.rb
@@ -4,25 +4,43 @@ module Mobile
   module V0
     module Concerns
       class Filter
-        attr_reader :records, :filters
-
-        def initialize(records, filters)
+        PERMITTED_OPERATIONS = %q[orEqual andEqual notEqual]
+        attr_reader :records
+        def initialize(records)
           @records = records
-          @filters = filters
         end
 
-        def self.filter(records, filters = {})
-          filters = filters.each(&:to_unsafe_hash)
+        def call(filter_params)
+          filters = filter_params.map(&:to_unsafe_hash)
+          filters = gather_filters(filters)
+          filters = validate_request(filters)
+          filter_records(filters)
+        end
 
+        def self.filter(records, filter_params = {})
           return [] if records.blank?
 
-          filterer = new(records, filters)
-          # filterer.validate_request
-          filterer.filter
-          records
+          filterer = new(records, filter_params)
+          filterer.call
+          filterer.records
         end
 
-        def validate_request
+        def gather_filters(filters)
+          filters.each_with_object({}) do |requested_params, received_filters|
+            filter_name = requested_params.keys.first
+            operation = requested_params[filter_name].keys.first
+            search_term = requested_params[filter_name][operation]
+            filter_pair = { operation: operation, search_term: search_term }
+
+            if received_filters.key?(filter_name)
+              received_filters[filter_name] << filter_pair
+            else
+              received_filters[filtern_name] = [filter_pair]
+            end
+          end
+        end
+
+        def validate_request(filters)
           classes = records.map(&:class).uniq
           raise 'MIXED CLASSES' if classes.count != 1
           unless (classes.first.attribute_names & filters.keys.map(&:to_sym)).count == filters.keys.count
@@ -31,10 +49,8 @@ module Mobile
           raise 'BAD FILTERS' unless filters.values.all? { |v| v.class.in?([String, Integer]) }
         end
 
-        def filter
-          filters.each_pair do |key, value|
-            @records.filter! { |r| r[k.to_sym] == v }
-          end
+        def filter_records(filters)
+          records.filter! { |r| r[k.to_sym] == v }
         end
       end
     end
