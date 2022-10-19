3cd6969e23909f78387fc6c25acec6703a9ebe80
Mike Moyer <michael.moyer@va.gov>
2022-10-19 14:53:27 -0400

    Got a rudimentary message search call working

--- app/models/message_search.rb
+++ app/models/message_search.rb
@@ -8,10 +8,10 @@ class MessageSearch < Common::Base
   # redis_config REDIS_CONFIG[:secure_messaging_store]
 
   attribute :exact_match, Boolean
-  attribute :sender_name, String
+  attribute :sender, String
   attribute :subject, String
   attribute :category, String
-  attribute :recipient_name, String
+  attribute :recipient, String
   attribute :from_date, Common::UTCTime
   attribute :to_date, Common::UTCTime
   attribute :message_id, Integer
