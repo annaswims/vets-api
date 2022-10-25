7e1252f5d646439294aa22bef51558550acfae43
Kyle Soskin <kyle.soskin@adhocteam.us>
2022-10-25 13:33:44 -0400

    Moving things around, breaking supp claims stuff into its own file to keep rubocop happy

--- app/controllers/appeals_base_controller_v1.rb
+++ app/controllers/appeals_base_controller_v1.rb
@@ -2,6 +2,8 @@
 
 require 'caseflow/service'
 require 'decision_review_v1/service'
+require 'decision_review_v1/supplemental_claims'
+
 
 class AppealsBaseControllerV1 < ApplicationController
   include ActionController::Serialization
