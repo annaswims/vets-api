ae4730b665ea4ea4e398e749d9d31599540978c1
Kyle Soskin <kyle.soskin@adhocteam.us>
2022-11-10 12:58:54 -0500

    Moving to model

--- app/controllers/v0/decision_review_evidences_controller.rb
+++ app/controllers/v0/decision_review_evidences_controller.rb
@@ -1,40 +1,9 @@
 # frozen_string_literal: true
-require 'decision_review/utilities/pdf_validation/service'
 
 # Notice of Disagreement evidence submissions
 module V0
   class DecisionReviewEvidencesController < ApplicationController
     include FormAttachmentCreate
     FORM_ATTACHMENT_MODEL = DecisionReviewEvidenceAttachment
-    
-    def create
-      form_attachment_model = self.class::FORM_ATTACHMENT_MODEL
-      form_attachment = form_attachment_model.new
-      namespace = form_attachment_model.to_s.underscore.split('/').last
-      filtered_params = params.require(namespace).permit(:file_data, :password)
-      # is it either ActionDispatch::Http::UploadedFile or Rack::Test::UploadedFile
-      fd = filtered_params[:file_data]
-      pw = filtered_params[:password]
-      unless fd.class.name.include? 'UploadedFile'
-        raise Common::Exceptions::InvalidFieldValue.new('file_data', fd.class.name)
-      end
-  
-      form_attachment.set_file_data!(fd, pw)
-      form_attachment.save!
-      lighthouse_validation_errors = validate_pdf(fd).body
-      ret_val = lighthouse_validation_errors.key?('errors') ? lighthouse_validation_errors : form_attachment
-      render(json: ret_val)
-    end
-  
-    private
-  
-    def decision_review_pdf_service
-      DecisionReview::PdfValidation::Service.new
-    end
-  
-    def validate_pdf(file)
-      decision_review_pdf_service.validate_pdf_with_lighthouse(file)
-    end
-
   end
 end
--- app/models/decision_review_evidence_attachment.rb
+++ app/models/decision_review_evidence_attachment.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
+require 'decision_review/utilities/pdf_validation/service'
 
 # Files uploaded as part of a Notice of Disagreement submission that will be sent to Lighthouse upon form submission.
 # inherits from ApplicationRecord
 class DecisionReviewEvidenceAttachment < FormAttachment
   ATTACHMENT_UPLOADER_CLASS = DecisionReviewEvidenceAttachmentUploader
+
+  validate :validate_pdf
+
+  def validate_pdf
+    decision_review_pdf_service.validate_pdf_with_lighthouse(self.get_file)
+  end
+
+  private
+
+  def decision_review_pdf_service
+    DecisionReview::PdfValidation::Service.new
+  end
 end
