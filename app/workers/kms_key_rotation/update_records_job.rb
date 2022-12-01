0a5aa9231f5b546de55b828de42112c393d98e59
rccassity <rccassity@gmail.com>
2022-12-01 08:23:48 -0600

    Added comment where rotate_kms_key is defined

--- app/workers/kms_key_rotation/update_records_job.rb
+++ app/workers/kms_key_rotation/update_records_job.rb
@@ -7,6 +7,8 @@ module KmsKeyRotation
     def perform(json)
       record_identifiers = JSON.parse(json)
       record_identifiers['model'].constantize.find(record_identifiers['id']).rotate_kms_key!
+      # This calls config/initializers/kms_encrypted_monkeypatch.rb
+      # define_method("rotate_#{key_method}!") do
     end
   end
 end
