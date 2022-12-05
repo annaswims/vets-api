22224e3ec02917292aee789215d7ef2cce4ef61b
s-caso <sebastian.caso@accenturefederal.com>
2022-12-05 16:06:48 -0500

    rubocop fix

--- modules/meb_api/app/controllers/meb_api/v0/education_benefits_controller.rb
+++ modules/meb_api/app/controllers/meb_api/v0/education_benefits_controller.rb
@@ -79,7 +79,13 @@ module MebApi
         claimant_response = claimant_service.get_claimant_info
         claimant_id = claimant_response['claimant_id']
 
-        response = claimant_id == 0 ? { enrollment_certify_responses: [] } : enrollment_service.submit_enrollment(params[:education_benefit], claimant_id)
+        response = if claimant_id.zero?
+                     { enrollment_certify_responses: [] }
+                   else
+                     enrollment_service.submit_enrollment(
+                       params[:education_benefit], claimant_id
+                     )
+                   end
 
         render json: response, serializer: SubmitEnrollmentSerializer
       end
