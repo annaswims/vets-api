a5b54ff04bf6a75fa07b90466f845f7e135acd24
Holden Hinkle <holden@holdenhinkle.com>
2022-12-05 12:33:21 -0500

    fix rubocop complaints in modules/appeals_api/spec/factories/higher_level_reviews.rb

--- modules/appeals_api/spec/factories/higher_level_reviews.rb
+++ modules/appeals_api/spec/factories/higher_level_reviews.rb
@@ -6,10 +6,12 @@ FactoryBot.define do
     id { SecureRandom.uuid }
     api_version { 'V1' }
     auth_headers do
-      JSON.parse File.read "#{::Rails.root}/modules/appeals_api/spec/fixtures/v1/valid_200996_headers.json"
+      JSON.parse File
+        .read(::Rails.root.join(*'/modules/appeals_api/spec/fixtures/v1/valid_200996_headers.json'.split('/')))
     end
     form_data do
-      JSON.parse File.read "#{::Rails.root}/modules/appeals_api/spec/fixtures/v1/valid_200996.json"
+      JSON.parse File
+        .read(::Rails.root.join(*'/modules/appeals_api/spec/fixtures/v1/valid_200996.json'.split('/')))
     end
   end
 
@@ -17,10 +19,12 @@ FactoryBot.define do
     id { SecureRandom.uuid }
     api_version { 'V2' }
     auth_headers do
-      JSON.parse File.read "#{::Rails.root}/modules/appeals_api/spec/fixtures/v2/valid_200996_headers.json"
+      JSON.parse File
+        .read(::Rails.root.join(*'/modules/appeals_api/spec/fixtures/v2/valid_200996_headers.json'.split('/')))
     end
     form_data do
-      JSON.parse File.read "#{::Rails.root}/modules/appeals_api/spec/fixtures/v2/valid_200996.json"
+      JSON.parse File
+        .read(::Rails.root.join(*'/modules/appeals_api/spec/fixtures/v2/valid_200996.json'.split('/')))
     end
 
     trait :status_error do
@@ -32,14 +36,13 @@ FactoryBot.define do
     id { SecureRandom.uuid }
     api_version { 'V2' }
     auth_headers do
-      JSON.parse(
-        File.read(
-          "#{::Rails.root}/modules/appeals_api/spec/fixtures/v2/valid_200996_headers_extra.json"
-        )
-      ).transform_values(&:strip)
+      JSON.parse File
+        .read(::Rails.root.join(*'/modules/appeals_api/spec/fixtures/v2/valid_200996_headers_extra.json'.split('/')))
+        .transform_values(&:strip)
     end
     form_data do
-      JSON.parse File.read "#{::Rails.root}/modules/appeals_api/spec/fixtures/v2/valid_200996_extra.json"
+      JSON.parse File
+        .read(::Rails.root.join(*'/modules/appeals_api/spec/fixtures/v2/valid_200996_extra.json'.split('/')))
     end
   end
 
@@ -47,10 +50,12 @@ FactoryBot.define do
     id { SecureRandom.uuid }
     api_version { 'V2' }
     auth_headers do
-      JSON.parse File.read "#{::Rails.root}/modules/appeals_api/spec/fixtures/v2/valid_200996_headers_minimum.json"
+      JSON.parse File
+        .read(::Rails.root.join(*'/modules/appeals_api/spec/fixtures/v2/valid_200996_headers_minimum.json'.split('/')))
     end
     form_data do
-      JSON.parse File.read "#{::Rails.root}/modules/appeals_api/spec/fixtures/v2/valid_200996_minimum.json"
+      JSON.parse File
+        .read(::Rails.root.join(*'/modules/appeals_api/spec/fixtures/v2/valid_200996_minimum.json'.split('/')))
     end
   end
 end
